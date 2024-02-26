36e1cb7467186113505d63ea584f32502facee6f
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2024-02-26 12:40:35 +0530

    first commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+      const Selection =({applyColor})=>{
+      const [bgColor,setBgColor]=useState("");
+
+
+      const applyBgColor=()=>{
+      setBgColor(background);
+ }
+      const changeColor=()=>{
+      applyColor(applyBgColor);
+ }
+      return(
+      <div style={{  backgroundColor:bgColor, width:200, height:200, border:"1 px solid gray"}}onClick={changeColor}>
+        <p>Selection</p>
+      </div>
+ )
+ 
+}
